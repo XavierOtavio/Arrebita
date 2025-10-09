6a86fdcafc0d52e8180b0e3ecf006342dc240419
pv25962 <pv25962@alunos.estgv.ipv.pt>
2025-10-09 17:19:54 +0100

    2 Commit

--- django_test/bd2Arrebita/bd2Arrebita/settings.py
+++ django_test/bd2Arrebita/bd2Arrebita/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Arrebita',
     'Accounts',
     'Wines',
     'Events',
@@ -122,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'Static/'
+STATICFILES_DIRS = [BASE_DIR / "Static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
